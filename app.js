55590f1e18c4d3ad165912000d6640482bc1a91c
Jacob Roth <jacobroth@u.boisestate.edu>
2026-04-30 20:38:42 -0600

    Update app.js
    Combined some code to make it simpliar

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { dbMiddleware} = require('./bin/db');
 
 
 const indexRouter = require('./routes/index');
+const createError = require('http-errors');
 //add more handlers here
 
 const app = express();
@@ -27,18 +28,16 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+  console.error(err.stack);
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
-});
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).render('error', { message: 'We are currently experiencing technical difficulties. Please try again later.' });
+  res.render('error', {
+    message: err.status === 404 ? 'Page not found.' : 'We are currently experiencing technical difficulties.'
+  });
 });
 
 module.exports = app;
